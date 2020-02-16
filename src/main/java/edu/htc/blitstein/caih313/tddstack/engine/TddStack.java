22b379ab363f4a1d6e3308383aae31d448f9d006
cliberman <chanaliberman1@gmail.com>
2020-02-16 14:46:51 -0600

    version 1 for hw 2

--- src/main/java/edu/htc/blitstein/caih313/tddstack/engine/TddStack.java
+++ src/main/java/edu/htc/blitstein/caih313/tddstack/engine/TddStack.java
@@ -1,4 +1,31 @@
 package edu.htc.blitstein.caih313.tddstack.engine;
 
-public class TddStack {
+import edu.htc.blitstein.caih313.tddstack.IStackable;
+
+public class TddStack implements IStackable {
+    static long DEFAULT_DEPTH = 100;
+    long stackDepth;
+
+    TddStack(long stackDepth) {
+    }
+
+    public TddStack() {
+
+    }
+
+    public static boolean isEmpty() {
+    return false;
+    }
+
+    public static boolean isFull() {
+    return false;
+    }
+
+    public void push(IStackable iStackable) {
+
+    }
+
+    public IStackable pop() {
+    return null;
+    }
 }
--- src/test/java/edu/htc/blitstein/caih313/tddstack/engine/TddStackTest.java
+++ src/test/java/edu/htc/blitstein/caih313/tddstack/engine/TddStackTest.java
@@ -1,7 +1,14 @@
 package edu.htc.blitstein.caih313.tddstack.engine;
 
+import edu.htc.blitstein.caih313.tddstack.resource.campus.Faculty;
+import edu.htc.blitstein.caih313.tddstack.IStackable;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TddStackTest {
 
@@ -12,4 +19,70 @@ public class TddStackTest {
     @After
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void testPublicConstructor() {
+        TddStack firstStack = new TddStack();
+        assertEquals(100, firstStack.DEFAULT_DEPTH);
+    }
+
+    @Test
+    public void testIsEmpty() {
+        TddStack firstStack = new TddStack();
+        assertEquals(true, firstStack.isEmpty());
+    }
+
+    @Test
+    public void testIsFull() {
+        TddStack firstStack = new TddStack();
+        assertEquals(false, firstStack.isFull());
+    }
+
+    @Test
+    public void testPop() {
+        TddStack firstStack = new TddStack();
+        assertNull(firstStack);
+    }
+
+    @Test
+    public void testPushThenNotEmpty() {
+        TddStack firstStack = new TddStack();
+        IStackable iStackable1 = new Faculty();
+        firstStack.push(iStackable1);
+        assertEquals(false, firstStack.isEmpty());
+    }
+
+    @Test
+    public void testFullWithThree() {
+        TddStack firstStack = new TddStack(3);
+        IStackable iStackable1 = new Faculty();
+        firstStack.push(iStackable1);
+        IStackable iStackable2 = new Faculty();
+        firstStack.push(iStackable2);
+        IStackable iStackable3 = new Faculty();
+        firstStack.push(iStackable3);
+        assertEquals(true, firstStack.isFull());
+    }
+
+    @Test
+    public void testPopIsPush() {
+        TddStack tddStack = new TddStack();
+        //Faculty implements IStackable
+        IStackable iStackableOrig = new Faculty();
+        tddStack.push(iStackableOrig);
+        IStackable iStackablePopped = tddStack.pop();
+        assertEquals(iStackableOrig, iStackablePopped);
+    }
 }
+
+
+
+/*Navigate to TddStackTest and implement the following tests:
+check that creating a TddStack with the public constructor sets the stackDepth to DEFAULT_DEPTH (not 100, but TddStack.DEFAULT_DEPTH).
+check that calling isEmpty() on a newly created stack returns "true"
+check that calling isFull() on a newly created stack returns "false"
+check that calling pop() on a newly created stack returns "null"
+For the next few tests, you'll need to instantiate a TddStack. You will also need to instantiate one or more IStackable objects.
+check that when a IStackable is pushed on the TddStack, then isEmpty() returns false
+Instantiate a TddStack with a depth of 3. Implement a test that checks that isFull() returns true when three IStackable objects are pushed onto it
+Check into git and give me the link to your git project.*/
